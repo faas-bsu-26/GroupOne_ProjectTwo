57905d8fdc1ccb3f9a34e126a34d4cf17f75411d
cburke6897 <cburke6897@gmail.com>
2026-04-07 16:40:07 -0400

    Removes css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
 import Home from './pages/Home'
 import Groups from './pages/Groups'
 import Activities from './pages/Activities'
