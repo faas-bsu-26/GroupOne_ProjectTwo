6b9eceb0e8a8780a57e86f6af6d9b72391daa460
cburke6897 <cburke6897@gmail.com>
2026-04-07 16:53:06 -0400

    Creates Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import HomeButton from "./HomeButton";
+import MenuDropdown from "./MenuDropdown";
+
+export default function Title({ title }) {
+    return (
+        <div className="flex items-center justify-between p-2 border-b border-text-light dark:border-text-dark">
+            <HomeButton />
+            <h1 className="text-2xl font-bold text-text-light dark:text-text-dark">{title}</h1>
+            <MenuDropdown />
+        </div>
+    );
+}
