043fdeac3f38e1476dedbb9f822f8d4de4decd3a
cburke6897 <cburke6897@gmail.com>
2026-04-07 15:47:16 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Groups from './pages/Groups'
+import Activities from './pages/Activities'
+import Community from './pages/Community'
+import Log from './pages/Log'
+import Scan from './pages/Scan'
+
+function App() {
+  return (
+    <Routes>
+      <Route path = "/" element = {<Home/>}/>
+      <Route path = "/groups" element = {<Groups/>}/>
+      <Route path = "/activities" element = {<Activities/>}/>
+      <Route path = "/community" element = {<Community/>}/>
+      <Route path = "/log" element = {<Log/>}/>
+      <Route path = "/scan" element = {<Scan/>}/>
+    </Routes>
+  )
+}
+
+export default App
