3556f8cc0f1bd629a140550bf7315c70ccdb9a79
Mitchell Williams <168118315+MitchellWilliams8@users.noreply.github.com>
2026-04-14 16:19:08 -0400

    Shows additional details of activities when clicked

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -1,7 +1,6 @@
 import Title from "../components/Title";
 import ActivityRow from "../components/ActivityRow";
 
-
 export default function Activities() {
     return (
         <div className="mx-auto min-h-screen flex flex-col max-w-100 bg-background-light text-text-dark dark:bg-background-dark dark:text-text-light">
@@ -10,36 +9,50 @@ export default function Activities() {
             <div className="p-4 flex flex-col gap-4">
 
                 <ActivityRow
-                name="Nature Walk"
-                date="April 22, 2026. 2 - 3 pm"
-                info="A walk threw nature."
-                image="NatureWalk.jpg"
-
+                    name="Nature Walk"
+                    date="April 22, 2026. 2 - 3 pm"
+                    info="Lorem ipsum dolor sit amet."
+                    image="NatureWalk.jpg"
+                    meetingPoint="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    organizer="Lorem ipsum dolor sit."
+                    details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    spotsLeft={8}
                 />
 
                 <ActivityRow
-                name="Nature Walk"
-                date="April 24, 2026. 2 - 3 pm"
-                info="A walk through nature."
-                image="NatureWalk.jpg"
+                    name="Nature Walk"
+                    date="April 24, 2026. 2 - 3 pm"
+                    info="Lorem ipsum dolor sit amet."
+                    image="NatureWalk.jpg"
+                    meetingPoint="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    organizer="Lorem ipsum dolor sit."
+                    details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    spotsLeft={8}
                 />
 
                 <ActivityRow
-                name="Nature Walk"
-                date="April 30, 2026. 5 - 6 pm"
-                info="A walk through nature."
-                image="NatureWalk.jpg"
+                    name="Nature Walk"
+                    date="April 30, 2026. 5 - 6 pm"
+                    info="Lorem ipsum dolor sit amet."
+                    image="NatureWalk.jpg"
+                    meetingPoint="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    organizer="Lorem ipsum dolor sit."
+                    details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    spotsLeft={8}
                 />
 
                 <ActivityRow
-                name="Nature Walk"
-                date="May 5, 2026. 5 - 6 pm"
-                info="A walk through nature."
-                image="NatureWalk.jpg"
+                    name="Nature Walk"
+                    date="May 5, 2026. 5 - 6 pm"
+                    info="Lorem ipsum dolor sit amet."
+                    image="NatureWalk.jpg"
+                    meetingPoint="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    organizer="Lorem ipsum dolor sit."
+                    details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    spotsLeft={8}
                 />
+
             </div>
         </div>
-
-      
     );
 }
