9562935b40621cca47f801df67cdc818ee04ae47
cburke6897 <cburke6897@gmail.com>
2026-04-20 09:35:49 -0400

    Adds basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/GroupOne_ProjectTwo'>
       <App />
     </BrowserRouter>
   </StrictMode>,
